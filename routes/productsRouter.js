8d1d85830f61611e51a3c67b6374c4edc73fff97
Aman Bansal <amanbansal@Aman-MacBook-Pro.local>
2025-11-09 18:49:44 +0530

    feat:route

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const productModel = require("../models/productModel")
 
 router.get("/", function (req, res) {
     res.send("Hey Working");
