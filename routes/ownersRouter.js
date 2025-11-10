1d3e164b0ca50975750fe0958cf534a3a227e6c0
Aman Bansal <amanbansal@Aman-MacBook-Pro.local>
2025-11-10 18:21:06 +0530

    feat: initial

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -6,4 +6,12 @@ router.get("/", function (req, res) {
     res.send("Hey Working");
 })
 
+if (process.env.NODE_ENV === 'development') {
+    router.post("/create", async function (req, res) {
+        let owners = await ownerModel.find();
+        // res.send("Hey Working");
+    })
+}
+
+
 module.exports = router;
