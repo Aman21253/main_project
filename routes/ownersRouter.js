59dfe7ffdee50254d33aca52e967b2f3edad6fcc
Aman Bansal <amanbansal@Aman-MacBook-Pro.local>
2025-12-01 19:55:33 +0530

    feat: owner and development

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -3,13 +3,25 @@ const router = express.Router();
 const ownerModel = require("../models/ownerModel")
 
 router.get("/", function (req, res) {
-    res.send("Hey Working");
+    res.send("Hey its Working");
 })
 
 if (process.env.NODE_ENV === 'development') {
     router.post("/create", async function (req, res) {
         let owners = await ownerModel.find();
-        // res.send("Hey Working");
+        if (owners.length > 0) {
+            return res
+                .send(503)
+                .send("You dont have permission becz owner exists");
+        }
+
+        let { fullName, email, password } = req.body;
+        let createdOwner = await ownerModel.create({
+            fullName,
+            email,
+            password,
+        });
+        res.status(201).send(createdOwner);
     })
 }
 
